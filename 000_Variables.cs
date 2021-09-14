5c5144dfefcc9909b2dd27b2e9d981cc51c40d78
Radka Pavlíčková <74163472+Pa-Rada@users.noreply.github.com>
2021-09-14 06:47:25 +0000

    Update and rename 000_Promenne.cs to 000_Variables.cs

--- 000_Variables.cs
+++ 000_Variables.cs
@@ -1,19 +1,19 @@
 using System;
 
-namespace 000_Promenne
+namespace 000_Variables
 {
   class Program
   {
     static void Main(string[] args)
     {
-      #Přiřazení hodnoty proměnné při její deklaraci:
+      // Přiřazení hodnoty proměnné při její deklaraci:
       int myAge = 25;
-      #
-      #Přiřazení hodnoty proměnné později:
+      
+      // Přiřazení hodnoty proměnné později:
       int myAge;
       myAge = 25;
-      #
-      #Datové typy proměnných:
+      
+      // Datové typy proměnných:
       int number = 42;
       float y = 4.2;
       double pi = 3.14;
